c297541afa1e33b038835ef01a9fe2cd9520b071
Kim Lodfelt <kim.lodfelt@dynabyte.se>
2012-11-05 16:26:43 +0100

    committing to go home

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -48,27 +48,26 @@ class CommentsController < ApplicationController
 
   def update
     @comment = Comment.find(params[:id])
-
+    @article = @comment.article
     respond_to do |format|
       if @comment.update_attributes(params[:comment])
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
+        format.html { redirect_to(@article, :notice => 'Comment was successfully updated.') }
+        format.xml  { head :ok }
       else
-        format.html { render action: "edit" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment = Comment.find(params[:id])
+    @article = Article.find(params[:article_id])
     @comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to comments_url }
-      format.json { head :no_content }
+      format.html { redirect_to(@article, :notice => 'Comment was successfully deleted.') }
+      format.xml  { head :ok }
     end
   end
 end
