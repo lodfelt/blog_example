62573d5df3a6aab708011318217b8fef6f21b47c
Kim Lodfelt <lodfelt@Kim-Lodfelts-MacBook.local>
2012-12-05 15:00:28 +0100

    wip mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Blog::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -35,5 +35,11 @@ Blog::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  if Rails.env.test? or Rails.env.cucumber?
+    config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  else
+    config.action_mailer.default_url_options = { host: 'http://www.lodfelt.se' }
+  end
+
+
 end
